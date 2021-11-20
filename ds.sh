44fa7add76ddc9312c93451af2fbbb579763733a
Anton Nikolov <58844584+ddevil345@users.noreply.github.com>
2021-11-20 11:44:48 +0200

    Update ds.sh

--- ds.sh
+++ ds.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 kubectl patch svc kubernetes-dashboard --type='json' -p '[{"op":"replace","path":"/spec/type","value":"LoadBalancer"}]' -n kubernetes-dashboard
+kubectl get secret -n kubernetes-dashboard $(kubectl get serviceaccount admin-user -n kubernetes-dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode >> /k8sinfo/ds.token
